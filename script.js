32c6af5712e97d960d7648949c57d214a400c0a0
justindclegg <justindclegg@gmail.com>
2016-05-27 23:38:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const _ = require('lodash');
-const Script = require('JustinBot').Script;
+const Script = require('smooch-bot').Script;
 
 const scriptRules = require('./script.json');
 
